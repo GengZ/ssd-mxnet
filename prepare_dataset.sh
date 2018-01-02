7afe343f81fb9199d1424e340498651a781988f7
GengZ <934113912@qq.com>
2018-01-02 13:49:15 +0800

    run for vid

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-python tools/prepare_dataset.py --dataset imagenet_vid --set random --root ~/vid_data/ILSVRC2015 --target ./data/train.lst
+python tools/prepare_dataset.py --dataset imagenet_vid --set random --root ~/Deep-Feature-Flow/data/ILSVRC2015 --target ./data_end_test/train.lst
+echo Done!
